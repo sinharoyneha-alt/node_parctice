a4e3b8098fed05827ebdbcc81f2a4d46847ef50f
sinharoyneha-alt <sinharoyneha@gmail.com>
2026-08-22 17:20:15 +0530

    api

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.post('/', async(req, res) => {
             const {name,email,password}=req.body;
             if(!name || !email || !password){
                 return res.status(400).json({
-                    message:"All fields are required"
+                    message:"All field are required"
                 });
             }
             res.status(200).json({
